5c524e417ddb78e2d333f161a11db42b61b69ae9
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-05-29 18:15:33 +0530

    creating cart ithems and orders

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("6648bde63a0051a06e7bac85")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
